43b26e2a41e90018bacf4657e8ee39a85183fe52
beesgab <yvesjeanlouisg@gmail.com>
2023-05-10 21:54:06 +0800

    Initial author routes

---
+++ routes/authors.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const router = express.Router()
+
+const Author = require('../models/author')
+
+//All authors route
+router.get('/', async (req,res)=>{
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== ''){
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try{
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query
+    })
+  }catch{
+    res.redirect('/')
+  }
+})
+
+//New Authors
+router.get('/new',(req,res)=>{
+  res.render('authors/new', {author: new Author()})
+})
+
+//create author route
+router.post('/', async (req,res)=>{
+  const author = new Author({
+    name: req.body.name
+  })
+  try{
+    const newAuthor = await author.save()
+    res.redirect('authors')
+  }catch{
+    res.render('authors/new', {
+      author: author,
+      errorMessage: "Error creating Author"
+    })
+  }
+
+  // author.save().then(()=>{
+  //     res.redirect('authors')
+  //   }).catch((e)=>{
+  //     res.render('authors/new', {
+  //       author: author,
+  //       errorMessage: "Error creating author.."  
+  //   })
+  // })
+
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,15 +6,17 @@ if (process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
-
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs') //view engine to use <%= variable > in the .ejs view
 app.set('views', __dirname + '/views') //add views link
 app.set('layout', 'layouts/layout') //add this for the views an ejs file which contain the header and footer, by then we can add <%- body %>
 app.use(expressLayouts)
 app.use(express.static('public')) //folder public for css, js and html
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 //connect to our mongodb database
 const mongoose = require('mongoose')//process.env is from .dotenv --> npm i --save-dev dotenv
@@ -24,8 +26,10 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to mongoose'))
 
-
-
+ 
+//for routes
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 
 app.listen(process.env.PORT || 3000)
